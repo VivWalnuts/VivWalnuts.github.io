c33b589493b9cf97d7f74c712d5de36f7f30dec0
VivWalnuts <shyguy64xyz@gmail.com>
2026-04-06 13:17:43 -0700

    tee

--- Testing/Testing.js
+++ Testing/Testing.js
@@ -1,8 +1,12 @@
 function setup() {
-
+createCanvas(600, 600);
+background(255, 23, 96)
 }
-
-
 function draw() {
-
+if (mouseIsPressed) {
+fill(0);
+} else {
+fill(255);
+}
+ellipse(mouseX, mouseY, 80, 80);
 }
